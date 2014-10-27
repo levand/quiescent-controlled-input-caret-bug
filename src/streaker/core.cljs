8cd579ff62f77b0177e284e6ef1342b267992d35
Dustin Getz <dustin.getz@gmail.com>
2014-10-27 18:38:33 -0400

    Clean up demo of caret jumping in controlled input without cursors

--- src/streaker/core.cljs
+++ src/streaker/core.cljs
@@ -1,36 +1,36 @@
 (ns streaker.core
   (:require [quiescent :as q :include-macros true]
-            [quiescent.dom :as d]
-            [cljs-cursor :refer [build-cursor refine set value]]))
+            [quiescent.dom :as d]))
 
 
-(q/defcomponent App [cursor]
+(defn App [state-atom refresh-fn]
   (d/div {:className "App"}
-         (d/pre {} (.stringify js/JSON (clj->js (value cursor)) nil 2))
-         (let [a (-> cursor
-                     (refine :app)
-                     (refine :a))]
-           (d/input {:value (value a)
-                     :onChange #(set a (-> % (.-value) (.-target)))}))))
+         (d/pre {} (.stringify js/JSON (clj->js @state-atom) nil 2))
+         (d/input {:value (get-in @state-atom [:app :a])
+                   :onChange (fn [e]
+                               (let [val (->> e (.-target) (.-value))]
+                                 (reset! state-atom
+                                         (assoc-in @state-atom [:app :a] val))
+                                 (refresh-fn)))})))
 
-(def initial-state {:app
-                    {:a "foo"}})
+
+(def initial-state {:app {:a "foo"}})
+
 
 (let [render-pending? (atom false)
       state (atom initial-state)]
-
   (defn request-render []
     (when (compare-and-set! render-pending? false true)
       (.setTimeout js/window
                    (fn []
-                     (q/render (App (build-cursor state request-render))
+                     (q/render (App state request-render)
                                (.getElementById js/document "root"))
                      (reset! render-pending? false))
-                   0)))
+                   0))))
 
 
-  (defn ^:export main []
-    (enable-console-print!)
-    (request-render)))
+(defn ^:export main []
+  (enable-console-print!)
+  (request-render))
 
 (main)
